817de4601a03f92790ba295f465b3c46851b2ab9
Nicolas Roeser <nicolas.roeser@uni-ulm.de>
2019-06-25 22:10:39 +0200

    Process weather data
    This solves the first part of the challenge.

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,8 @@
 package de.exxcellent.challenge;
 
+import java.io.InputStream;
+import java.io.IOException;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -8,18 +11,45 @@ package de.exxcellent.challenge;
  */
 public final class App {
 
+    private static final String WEATHER_RESOURCE_NAME = "weather.csv";
+    private static final String[] WEATHER_COLUMN_HEADINGS =
+        { "MnT", "MxT", "Day" };
+
+    /**
+     * Process all input and print output. Pass on I/O errors for handling by
+     * the caller.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    public static void processData() throws IOException {
+        InputStream resIn =
+                App.class.getResourceAsStream(WEATHER_RESOURCE_NAME);
+        CSVDataAnalyzer weatherAnalyzer =
+                new CSVDataAnalyzer(resIn,
+                        WEATHER_COLUMN_HEADINGS[0],
+                        WEATHER_COLUMN_HEADINGS[1],
+                        WEATHER_COLUMN_HEADINGS[2]);
+
+        String dayWithSmallestTempSpread = weatherAnalyzer.getMinSpreadLabel();
+
+        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+
+        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+    }
+
     /**
      * This is the main entry method of your program.
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
-
-        // Your preparation code …
-
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
-
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+        try {
+            processData();
+        } catch (IOException e) {
+            // XXX: we normally use a Logger for this, but we try to KISS.
+            System.err.println();
+            e.printStackTrace();
+            return;
+        }
     }
 }
